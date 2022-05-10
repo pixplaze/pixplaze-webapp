ffdd536af2ffa9d61519ce8fbb21af293363f166
emberati <alex.shak55@gmail.com>
2022-05-10 23:53:48 +0300

    Создана разметка страницы `HolygrailTemplate.vue`
    -	Создана разметка страницы по типу `Holy Grail`, реализованая в
    	компоненте `HolygraulTemplate.vue`.
    -	Начата реализация компонента шапки `HeaderNav.vue`.

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,27 @@
 <template>
-  <rcon-console/>
-  <section class="test">
-    <textured-button>Трава зелёная</textured-button>
-    <textured-button :variant="'stone'">Я каменб</textured-button>
-    <mc-button>Суперкек</mc-button>
-    <mc-button>Суперкек</mc-button>
-  </section>
-  <section class="test">
-    <menu-button/>
-  </section>
+  <holygrail-template>
+    <!-- <rcon-console/>
+    <section class="test">
+      <textured-button>Трава зелёная</textured-button>
+      <textured-button :variant="'stone'">Я каменб</textured-button>
+      <mc-button>Суперкек</mc-button>
+      <mc-button>Суперкек</mc-button>
+    </section>
+    <section class="test">
+      <menu-button/>
+    </section> -->
+  </holygrail-template>
 </template>
 
 <script>
-import RconConsole from '@/components/RconConsole'
+// import RconConsole from '@/components/RconConsole'
+import HolygrailTemplate from '@/components/HolygrailTemplate'
 
 export default {
   name: 'App',
   components: {
-    RconConsole
+    // RconConsole,
+    HolygrailTemplate
   }
 }
 </script>
