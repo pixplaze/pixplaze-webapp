55bec1fe5ab426e499c9b25ffb20df7fe9f36e64
emberati <alex.shak55@gmail.com>
2022-05-17 21:09:59 +0300

    Доделан слайдер, не адаптируется под размер

--- src/components/TriptychSlider.vue
+++ src/components/TriptychSlider.vue
@@ -2,7 +2,7 @@
 export default {
   name: "triptych-slider",
   props: {
-    images: {
+    items: {
       type: Array[String],
       default: [
         {
@@ -39,90 +39,78 @@ export default {
     }
   },
   data: () => ({
-    displayImages: null,
+    displayItems: null,
     frameElement: null,
+    innerElement: null,
+
     firstItem: null,
     lastItem: null,
-    innerElement: null,
+    
     slideDisabled: false,
 
-    itemWidth: 400,
+    itemWidth: 1000,
     listGap: 50,
   }),
   methods: {
     slideLeft() {
       this.slideDisabled = true
       
-
       this.innerElement.classList.toggle('slide-left')
-      let image = this.displayImages[this.displayImages.length-1]
-      this.displayImages.unshift(image)
+      let image = this.displayItems[this.displayItems.length-1]
+      this.displayItems.unshift(image)
+
       setTimeout(() => {
         this.innerElement.classList.toggle('slide-left')
-        this.displayImages.pop()
-
+        this.displayItems.pop()
         this.slideDisabled = false
       }, 500)
     },
-    async slideRight() {
+    slideRight() {
       this.slideDisabled = true
-      
-      this.firstItem = this.displayImages[this.displayImages.length -3]
-      this.lastItem = this.displayImages[this.displayImages.length -1]
 
       this.innerElement.classList.toggle('slide-right')
-      let image = this.displayImages[0]
-      this.displayImages.push(image)
+      let image = this.displayItems[0]
+      this.displayItems.push(image)
+      
       setTimeout(() => {
         this.innerElement.classList.toggle('slide-right')
-        image = this.displayImages.shift()
-        
+        image = this.displayItems.shift()
         this.slideDisabled = false
       }, 500)
     },
     setFirstItem() {
-      this.firstItem = this.displayImages[this.displayImages.length-1]
+      this.firstItem = this.displayItems[this.displayItems.length-1]
     },
     setLastItem() {
-      this.lastItem = this.displayImages[0]
+      this.lastItem = this.displayItems[0]
     }
   },
   computed: {
-    itemBoundsStyle() {
+    itemBoundsStyle() { 
+      let evenAddition = this.items.length % 2? 0 : this.itemWidth + this.listGap
+
+      let itemWidth = this.itemWidth
+      let itemGappedWidth = this.itemWidth + this.listGap
+      let outerWidth = this.itemWidth * this.items.length + this.listGap * (this.items.length - 1) - evenAddition
+      let outerMarginLeft = -itemGappedWidth * parseInt(this.items.length / 2)
+      let listGap = this.listGap
+
       return {
-        '--image-width': `${this.itemWidth}px`,
-        '--list-gap': `${this.listGap}px`
+        '--item-width': `${itemWidth}px`,
+        '--item-gapped-width': `${itemGappedWidth}px`,
+        '--outer-width': `${outerWidth}px`,
+        '--outer-margin-left': `${outerMarginLeft}px`,
+        '--list-gap': `${listGap}px`
       }
-    },
-    rightImage() {
-      // let index = 0
-      if (this.selectedIndex)
-      this.displayImages[this.selectedIndex]
-      return null
     }
   },
   mounted() {
-    this.displayImages = [...this.images]
+    this.displayItems = [...this.items]
     this.frameElement = this.$refs.frame
     this.innerElement = this.$refs.inner
 
     this.setFirstItem()
     this.setLastItem()
-    
-    // this.innerElement.style.left = `${this.frameElement.offsetLeft}px`
-
-    // this.innerElement.style.left = `-${this.itemWidth + this.listGap}px`
-  },
-  watch: {
-    images: {
-      handler() {
-        // this.innerElement.style.left = `-${this.itemWidth - this.listGap}px`
-        console.log(this.images)
-      }, deep: true
-    },
-    selectedIndex() {
-      console.log(this.selectedIndex);
-    }
   }
 }
 </script>
@@ -131,44 +119,36 @@ export default {
 <div class="slider" :style="itemBoundsStyle">
   <div ref="frame" class="slider-frame">
     <arrow-button :variant="'left'" :disabled="slideDisabled" @click="slideLeft"/>
-    <!-- <transition-group name="list-items"> -->
-      <div ref="inner" class="slider-inner">
-        <!-- <div v-if="displayImages && displayImages.length <= 3"></div> -->
-        <img 
-          class="item"
-          v-if="firstItem"
-          :src="firstItem.url">
-        <img
-          class="item"
-          
-          v-for="image, i in displayImages"
-          
-          :key="i"
-          :src="image.url"
-          :alt="image.alt">
-        <img 
-          class="item"
-          v-if="lastItem"
-          :src="lastItem.url">
-        <!-- <div class="item" style="background: red; width: var(--image-width)"></div> -->
+      <div class="slider-outer">
+        <div ref="inner" class="slider-inner">
+          <img 
+            class="item"
+            v-if="firstItem"
+            :src="firstItem.url"
+          >
+          <img
+            class="item"
+            v-for="image, i in displayItems"
+            :key="i"
+            :src="image.url"
+            :alt="image.alt"
+          >
+          <img 
+            class="item"
+            v-if="lastItem"
+            :src="lastItem.url"
+          >
+        </div>
       </div>
-    <!-- </transition-group> -->
     <arrow-button :variant="'right'" :disabled="slideDisabled" @click="slideRight"/>
   </div>
-  
 </div>
 </template>
 
 <style scoped>
 .slider {
   position: relative;
-  /* display: flex; */
-  
-  /* height: 300px; */
-}
-
-.arrow-button {
-
+  overflow: hidden;
 }
 
 .arrow-button.left {
@@ -191,48 +171,41 @@ export default {
 
 .slider-frame {
   content: "";
-  /* z-index: 1; */
+  z-index: 1;
   position: relative;
-  width: var(--image-width);
-  height: 100%;
+  width: var(--item-width);
 
-  /* left: 0;
-  right: 0; */
   margin-left: auto;
   margin-right: auto;
-
-  /* background-color: red; */
-  border: 1px solid red;
 }
 
 .slider-inner.slide-left {
   transition: transform .5s ease;
-  transform: translateX(450px);
-  margin-left: calc(calc(var(--image-width) + var(--list-gap)) * -3);
+  transform: translateX(var(--item-gapped-width));
+  margin-left: calc(-2 * var(--item-gapped-width));
 }
 
 .slider-inner.slide-right {
   transition: transform .5s ease;
-  transform: translateX(-450px);
+  transform: translateX(calc(-1 * var(--item-gapped-width)));
+}
+
+.slider-outer {
+  overflow: hidden;
+  left: 50%;
+  margin-left: var(--outer-margin-left);
+  width: var(--outer-width);
 }
 
 .slider-inner {
-  /* position: relative; */
-  display: inline-flex;
+  display: flex;
   gap: var(--list-gap);
-  /* left: 50%;
-  transform: translateX(-50%); */
-
-  
-  /* transform: translateX(-450px); */
-  margin-left: calc(calc(var(--image-width) + var(--list-gap)) * -2);
+  margin-left: calc(-1 * var(--item-gapped-width));
 }
 
 img.item {
-  /* position: relative; */
-  /* transform: scale(1); */
   transition: all .5s ease;
-  width: var(--image-width);
+  width: var(--item-width);
 }
 
 img.item.selected {
