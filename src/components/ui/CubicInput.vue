b559554e61f2d5a8af5728d69b705ae02daed67d
emberati <alex.shak55@gmail.com>
2023-08-02 19:54:31 +0300

    Minor layout fixes

---
+++ src/components/ui/CubicInput.vue
@@ -0,0 +1,30 @@
+<script>
+export default {
+  name: 'cubic-input',
+  props: {
+    id: {
+      type: String,
+      required: true
+    },
+    name: {
+      type: String,
+      required: true
+    },
+    type: {
+      type: String,
+      default: 'text'
+    }
+  }
+}
+</script>
+
+<template>
+  <input :id="id" :name="name" :type="type">
+</template>
+
+<style scoped>
+input {
+  height: 35px;
+  /* box-shadow: inset; */
+}
+</style>
